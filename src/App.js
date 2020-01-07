08b90a202db64422ad7ae66f77e6f535246a1415
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-07 15:05:52 -0500

    added form, button, title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Appx from './component/Appx'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div >
+      {/* <h1>hello</h1> */}
+      <Appx />
     </div>
   );
 }
---
+++ src/component/Appx.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Appx extends Component{
+    render(){
+        return (
+            <div className="App">
+                <div className="title">
+                Logger
+                </div>
+
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input className="form-control" placeholder="I have to..." />
+                    
+                    </div>
+
+                    <button type="button" className="btn btn-success"> Add Reminder
+
+
+
+
+                    </button>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Appx
